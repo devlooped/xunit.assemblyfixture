9ff6bbb42cd5e4f4a0f7e49c731949be0e55e8d7
Kirill Pivnenko <kirill.pivnenko@dazlab.io>
2023-06-29 13:12:17 +0600

    Add IAssemblyAsyncLifetime interface

---
+++ src/AssemblyFixture/IAssemblyAsyncLifetime.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+namespace Xunit;
+
+/// <summary>
+/// Used to provide asynchronous lifetime functionality per assembly.
+/// It is like xunit IAsyncLifetime, but per assembly
+/// </summary>
+public interface IAssemblyAsyncLifetime
+{
+    /// <summary>
+    /// Called before tests started in the assembly.
+    /// </summary>
+    Task InitializeAsync();
+
+    /// <summary>
+    /// Called when all tests finished in the assembly.
+    /// </summary>
+    Task DisposeAsync();
+}
